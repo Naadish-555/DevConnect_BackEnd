f114c38730a9edd37b2cfa9ae76b5e9b89d76e71
Nandish Mittal <Nandishmittal555@gmail.com>
2024-04-27 16:14:44 +0530

    Added a category schema along with all its CRUD operations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config() //header file
 require("./connectors/mongo.connector")
 const express = require("express");
 const projectRouter =require("./routes/project.routes");
+const categoryRouter = require("./routes/categories.routes");
 
 console.log(process.env.MONGO_CONNECTION_STRING);
 
@@ -11,6 +12,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use("/api",projectRouter);  
+app.use("/api",categoryRouter)
 //start the sever
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
